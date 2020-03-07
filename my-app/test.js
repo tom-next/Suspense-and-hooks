e91b2739fde90ec2c5f0503e92d455bc9112b330
Tom Li <tom.li@klook.com>
2020-03-07 17:25:23 +0800

    重构任务，改写为runner的形式

--- my-app/test.js
+++ my-app/test.js
@@ -1,41 +1,39 @@
 // 目的: 1. 当遇到异步副作用时，流程暂停，等待任务完成。当任务完成时候，继续执行流程
 let pos = 0
 
-const cache = []
+let cache = []
 
 const task1 = () => {
-    if(cache[pos]) {
-        // pos + 1 下一位, 同时返回当前值
-        return cache[pos++]
-    }else {
-        throw new Promise(resovle => setTimeout(() => {
-            resovle('task1')
-        }, 1000))
-    }
+    return wrapTask(new Promise(resovle => setTimeout(() => {
+        resovle('task1')
+    }, 1000)))
 }
 
 const task2 = () => {
-    if(cache[pos]) {
-        // pos + 1 下一位, 同时返回当前值
-        return cache[pos++]
-    }else {
-        throw new Promise(resovle => setTimeout(() => {
-            resovle('task2')
-        }, 1000))
-    }
+    return wrapTask(new Promise(resovle => setTimeout(() => {
+        resovle('task2')
+    }, 1000)))
 }
 
-const main = () => {
-    const step = () => {
+// 异步任务包一层，用来控制任务是否进入暂态
+const wrapTask = (task) => {
+    if(cache[pos]) {
+        return cache[pos++]
+    }
+    throw task
+}
+
+// ruuner 
+const runner = (process) => {
+    cache = []
+    function step() {
         pos = 0
         try {
-            let ret1 = task1()
-            let ret2 = task2()
-            console.log("result", ret1, ret2)
-        } catch (task) {
-            task.then((value) => {
-                cache[pos++] = value
-                // 重启流程
+            const ret = process()
+            return ret
+        } catch(task) {
+            return task.then((value) => {
+                cache[pos] = value
                 return step()
             })
         }
@@ -43,4 +41,11 @@ const main = () => {
     return step()
 }
 
-main()
+
+const main = () => {
+    const ret1 = task1()
+    const ret2 = task2()
+    console.log("result is", ret1, ret2)
+}
+
+runner(main)
